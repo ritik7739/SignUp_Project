bce87307d2218799908cbb37028fdfba706c0269
Ritik kumar singh <102001336+ritik7739@users.noreply.github.com>
2024-06-06 16:45:51 +0530

    db.js

--- config/db.js
+++ config/db.js
@@ -7,14 +7,15 @@ dotenv.config();
 const MONGO_URL = process.env.MONGO_URL;
 
 
-const connectToDatabase = () => {
-    mongoose.connect(MONGO_URL)
-        .then((con) => {
-            console.log(`Database connected successfully: ${con.connection.host}`);
-        })
-        .catch((err) => {
-            console.log(`Error detected while connecting to database: ${err}`);
-        });
+const connectToDatabase = async () => {
+    try {
+        const con = await mongoose.connect(MONGO_URL);
+        console.log(`Database connected successfully: ${con.connection.host}`);
+    } catch (err) {
+        console.log(`Error detected while connecting to database: ${err}`);
+    }
+};
+
 };
 
 export default connectToDatabase; // Exporting the function as default
